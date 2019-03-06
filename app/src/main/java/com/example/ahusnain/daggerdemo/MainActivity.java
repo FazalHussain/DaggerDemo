f9d4e8d3624c61ecfb2da0de562b6d13396f6571
ahusnain <ahussnain.ka@emaar.ae>
2019-03-06 17:23:22 +0400

    Dagger interface dependency using binds instead of provide

--- app/src/main/java/com/example/ahusnain/daggerdemo/MainActivity.java
+++ app/src/main/java/com/example/ahusnain/daggerdemo/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.ahusnain.daggerdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.ahusnain.daggerdemo.car.Car;
+import com.example.ahusnain.daggerdemo.dagger.CarComponent;
+import com.example.ahusnain.daggerdemo.dagger.WheelModule;
+
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CarComponent carComponent = DaggerCarComponent.create();
+        CarComponent carComponent = DaggerCarComponent.builder().
+                wheelModule(WheelModule.INSTANCE).
+                build();
         carComponent.inject(this);
 
         car.drive();
